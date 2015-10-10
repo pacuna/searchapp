71f54a96dc1dee1cf9bb55ef2ab99be15892c189
Pablo Acuña <pabloacuna88@gmail.com>
2015-10-10 15:33:25 -0300

    add healthcheck route

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,6 @@ Rails.application.routes.draw do
     collection { get :search }
   end
 
-  get '/healthcheck', to: 'pages#healthcheck'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
