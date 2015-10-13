2cdcb9d48ba21b2d47d32d4822cc7dc10e544a08
Pablo Acuña <pabloacuna88@gmail.com>
2015-10-12 21:36:50 -0300

    add redis container for testing environment

--- deploy/ci_setup.sh
+++ deploy/ci_setup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 docker pull mysql:5.7
 docker pull elasticsearch
+docker pull redis
 docker run --name mysql -e MYSQL_ROOT_PASSWORD=secretpassword -d mysql:5.7
 docker run --name elasticsearch -d elasticsearch
+docker run --name redis -d redis
 
 sleep 15s
-docker run --name searchapp-test -e PASSENGER_APP_ENV=test --link mysql:db --link elasticsearch:elasticsearch --entrypoint="./deploy/run_tests.sh" -t pacuna/searchapp:test_v_${BUILD_NUMBER} | perl -pe '/Tests failed./ && `echo -n "Tests failed" > tests-failed-flag`'
+docker run --name searchapp-test -e PASSENGER_APP_ENV=test --link mysql:db --link elasticsearch:elasticsearch --link redis:redis --entrypoint="./deploy/run_tests.sh" -t pacuna/searchapp:test_v_${BUILD_NUMBER} | perl -pe '/Tests failed./ && `echo -n "Tests failed" > tests-failed-flag`'
 if [ ! -f tests-failed-flag ]; then
   echo -e "Tests passed."
 else
@@ -15,9 +17,11 @@ else
   docker rm -f searchapp-test
   docker rm -f mysql
   docker rm -f elasticsearch
+  docker rm -f redis
   exit 1
 fi
 echo 'removing test containers'
 docker rm -f searchapp-test
 docker rm -f mysql
 docker rm -f elasticsearch
+docker rm -f redis
