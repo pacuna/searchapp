1bc344f815bed5ebe0506d24ef7075a28900bc16
Pablo Acuña <pabloacuna88@gmail.com>
2015-10-12 21:22:24 -0300

    add ci files

---
+++ deploy/ci_setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+docker pull mysql:5.7
+docker pull elasticsearch
+docker run --name mysql -e MYSQL_ROOT_PASSWORD=secretpassword -d mysql:5.7
+docker run --name elasticsearch -d elasticsearch
+
+sleep 15s
+docker run --name searchapp-test -e PASSENGER_APP_ENV=test --link mysql:db --link elasticsearch:elasticsearch --entrypoint="./deploy/run_tests.sh" -t pacuna/searchapp:test_v_${BUILD_NUMBER} | perl -pe '/Tests failed./ && `echo -n "Tests failed" > tests-failed-flag`'
+if [ ! -f tests-failed-flag ]; then
+  echo -e "Tests passed."
+else
+  echo -e "Build failed since tests failed."
+  rm tests-failed-flag
+  echo 'removing test containers'
+  docker rm -f searchapp-test
+  docker rm -f mysql
+  docker rm -f elasticsearch
+  exit 1
+fi
+echo 'removing test containers'
+docker rm -f searchapp-test
+docker rm -f mysql
+docker rm -f elasticsearch
---
+++ deploy/run_tests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo 'creating test database...'
+RAILS_ENV=test rake db:create
+echo 'migrating test database...'
+RAILS_ENV=test rake db:migrate
+echo 'running tests...'
+RAILS_ENV=test rspec
+
+return_code=$?
+if [[ $return_code != 0 ]] ; then
+  echo -e "Tests failed."
+  exit $return_code
+fi
+
