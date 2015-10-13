c812cc61199a699ff0ae4ebe7f91e60fd0603597
Pablo Acuña <pabloacuna88@gmail.com>
2015-10-12 21:40:25 -0300

    fix es alias for testing container

--- deploy/ci_setup.sh
+++ deploy/ci_setup.sh
@@ -7,7 +7,7 @@ docker run --name elasticsearch -d elasticsearch
 docker run --name redis -d redis
 
 sleep 15s
-docker run --name searchapp-test -e PASSENGER_APP_ENV=test --link mysql:db --link elasticsearch:elasticsearch --link redis:redis --entrypoint="./deploy/run_tests.sh" -t pacuna/searchapp:test_v_${BUILD_NUMBER} | perl -pe '/Tests failed./ && `echo -n "Tests failed" > tests-failed-flag`'
+docker run --name searchapp-test -e PASSENGER_APP_ENV=test --link mysql:db --link elasticsearch:es --link redis:redis --entrypoint="./deploy/run_tests.sh" -t pacuna/searchapp:test_v_${BUILD_NUMBER} | perl -pe '/Tests failed./ && `echo -n "Tests failed" > tests-failed-flag`'
 if [ ! -f tests-failed-flag ]; then
   echo -e "Tests passed."
 else
